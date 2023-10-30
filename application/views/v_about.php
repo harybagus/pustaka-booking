a6bb6547e97fb928c0c4d01be669175889dfc87a
Bagus Hary <bagushary79@gmail.com>
2023-10-30 22:30:08 +0700

    Pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?= $judul; ?></h1>
+
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan: Bagus</li>
+        <li>Nama Belakang: Hary</li>
+    </ul>
+
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jl. jalan</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Depok</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Futsal</li>
+        <li>Kempo</li>
+    </ul>
+</section>
